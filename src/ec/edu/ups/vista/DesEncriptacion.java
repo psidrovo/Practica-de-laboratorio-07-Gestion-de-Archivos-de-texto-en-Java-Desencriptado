76a76b19c9214472f16945986e2b23fa511bb5c1
Denys <adriandutan15@gmail.com>
2020-07-12 14:40:33 -0500

    AREGGLOS MENORES

--- src/ec/edu/ups/vista/DesEncriptacion.java
+++ src/ec/edu/ups/vista/DesEncriptacion.java
@@ -131,11 +131,11 @@ public class DesEncriptacion extends javax.swing.JFrame {
 
     /**
      * OPCION MENU ABRIR. Mediante el evento de action performed se ejecuta el
-     * JFileChooser el mismo que nos ayudara a buscar la ruta de donde deseamos
+     * ruta a abrir el mismo que nos ayudara a buscar la ruta de donde deseamos
      * guardar. Este se puede personalizar segun nuestras necesidades, en este
      * caso fc.setDialogTitle("") se le pasa un titulo dependiendo la necesidad
      * del programador, en este caso es BUSCAR DOCUMENTO.
-     * fc.setCurrentDirectory(File) se le pasa ob objeto tipo file que debe
+     * fc.setCurrentDirectory(File) se le pasa objeto tipo file que debe
      * tener la ruta donde deseamos que se abra como predeterminado, en caso de
      * no encontrarla se ira a la ruta predeterminada del sistema de Documentos.
      * fc.setFileFilter(FileNameExtensionFilter) se le pasa el tipo de archivos
